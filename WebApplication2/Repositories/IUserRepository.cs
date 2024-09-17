6baf61d78a3eaa5026696e235637fce3f2d6b87e
LORENZO LOTERO GAITAN <llotero@ucundinamarca.edu.co>
2024-09-17 18:59:47 -0500

    UserRepository

---
+++ WebApplication2/Repositories/IUserRepository.cs
@@ -0,0 +1,45 @@
+﻿using SGME.Model;
+
+
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllUserAsync();
+        Task<User> GetUserByIdSync(int id);
+        Task CreateUserAsync(User user);
+        Task UpdateUserAsync (User user);
+        Task SoftDeleteUser(int id);
+    }
+
+public class UserRepository : IUserRepository
+
+
+{
+    public UserRepository()
+    {
+        
+    }
+    public Task CreateUserAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<User>> GetAllUserAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetUserByIdSync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SoftDeleteUser(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateUserAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+}
