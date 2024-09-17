c8dce450c1bda1ce7964b290dcbec7b1b0e3c738
ÁNGEL CAMILO RIVERA LEAL <angelcrivera@ucundinamarca.edu.co>
2024-09-17 18:51:25 -0500

    Okey

---
+++ WebApplication2/Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using WebApplication2.Model;
+
+namespace SGME.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllUserAsync();
+        Task<User> GetUserByIdAsync(int id);
+        Task CreateUserAsync(User user);
+        Task UpdateUserAsync(User user);
+        Task SoftDeleteUserAsync(int id);
+
+
+    }
+}
