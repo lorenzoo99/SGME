08ce25279c8d77deadfd592c4c1060b80498a7f8
ÁNGEL CAMILO RIVERA LEAL <angelcrivera@ucundinamarca.edu.co>
2024-09-17 19:05:32 -0500

    Clase 17092024

--- WebApplication2/Repositories/UserRepository.cs
+++ WebApplication2/Repositories/UserRepository.cs
@@ -12,4 +12,36 @@ namespace SGME.Repositories
 
 
     }
+
+    public class UserRepository : IUserRepository
+    {
+        public UserRepository()
+        {
+            
+        }
+        public Task CreateUserAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetAllUserAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SoftDeleteUserAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateUserAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
